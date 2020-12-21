7aa492f0c5cf7c74e4bb548f952685de0bf6c7cf
jconeff <jessicaconeff@gmail.com>
2020-12-21 17:54:26 -0500

    changed paths

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,49 +1,81 @@
-function renderBadge(license) {
-  if (license == 'MIT') {
-      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+const generateInstall = installText => {
+    if (!installText) {
+      return ''
+    } else {
+      return `## Installation
+    ${installText}
+    `
+    }
   }
-  if (license == 'Apache 2.0') {
-      return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+  const generateContribute = contributeText => {
+    if (!contributeText) {
+      return ''
+    } else {
+      return `## Contributors 
+    ${contributeText}
+    `
+    }
   }
-  if (license == 'Creative Commons 1.0') {
-      return "[![License: CC0-1.0](https://img.shields.io/badge/License-CC0%201.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)"
+  const generateTesting = testText => {
+    if (!testText) {
+      return ''
+    } else {
+      return `## Testing
+    ${testText}
+    `
+    }
   }
-  if (license == 'GPLv3') {
-      return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
+  const installCheck = check => {
+    if (!check) {
+      return '';
+    } else {
+      return `* [Installation](#installation)`
+    }
   }
-  if (license == 'WTFPL') {
-      return "[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)"
+  const contributeCheck = check => {
+    if (!check) {
+      return '';
+    } else {
+      return `* [Contributions](#contributions)`
+    }
   }
-};
-
-// Function to generate markdown for README
-function generateMarkdown(data) {
-  return `
-  ![GitHub](https://img.shields.io/github/license/${data.githubUsername}/${data.title})
-  # ${data.title}
-  ## Description
-  ${data.about}
-  ## Table of Contents
-  ${installCheck(data.install)}
-  * [Usage](#usage)
-  * [License](#license)
-  ${contributeCheck(data.contribution)}
-  ${testCheck(data.testing)}
-  * [Questions](#questions)
+  const testCheck = check => {
+    if (!check) {
+      return '';
+    } else {
+      return `* [Testing](#testing)`
+    }
+  }
   
-  ${generateInstall(data.install)}
-  ## Usage
-  ${data.usage}
-  ## License
-  * This application is covered under the ${data.license} license
-  ${generateContribute(data.contribution)}
-  ${generateTesting(data.testing)}
-  ## Questions
-  Created by: [${data.githubUsername}](${data.gitLink})
   
-  If you have further questions please feel free to contact me at [${data.email}](${data.email})
-`;
-}
-
-module.exports = generateMarkdown;
-
+  
+  // function to generate markdown for README
+  function generateMarkdown(data) {
+    return `
+    ![GitHub](https://img.shields.io/github/license/${data.githubUsername}/${data.title})
+    # ${data.title}
+    ## Description
+    ${data.about}
+    ## Table of Contents
+    ${installCheck(data.install)}
+    * [Usage](#usage)
+    * [License](#license)
+    ${contributeCheck(data.contribution)}
+    ${testCheck(data.testing)}
+    * [Questions](#questions)
+    
+    ${generateInstall(data.install)}
+    ## Usage
+    ${data.usage}
+    ## License
+    * This application is covered under the ${data.license} license
+    ${generateContribute(data.contribution)}
+    ${generateTesting(data.testing)}
+    ## Questions
+    Created by: [${data.githubUsername}](${data.gitLink})
+    
+    If you have any further questions please feel free to contact me at [${data.email}](${data.email})
+  `;
+  }
+  
+  module.exports = generateMarkdown;
